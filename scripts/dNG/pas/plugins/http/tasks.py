7370aa9c9daab635ed5912d59a2f58badc225c6b
@4u <web@direct-netware.de>
2013-06-12 06:19:12 +0200

    Initial commit

---
+++ scripts/dNG/pas/plugins/http/tasks.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+dNG.pas.plugins.http.tasks
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?pas;http;tasks
+
+The following license agreement remains valid unless any additions or
+changes are being made by direct Netware Group in a written form.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;gpl
+----------------------------------------------------------------------------
+#echo(pasHttpTasksVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+from dNG.pas.controller.predefined_http_request import direct_predefined_http_request
+from dNG.pas.data.http.virtual_config import direct_virtual_config
+from dNG.pas.data.logging.log_line import direct_log_line
+from dNG.pas.plugins.hooks import direct_hooks
+
+def direct_plugin_http_startup(params, last_return):
+#
+	"""
+Called for "dNG.pas.http.startup" and "dNG.pas.http.wsgi.startup"
+
+:param params: Parameter specified
+:param last_return: The return value from the last hook called.
+
+:since:  v0.1.00
+	"""
+
+	direct_virtual_config.set_virtual_path("/tasks/", { "uri": "tid", "uri_prefix": "/tasks/" }, direct_plugin_handle_http_request)
+	return last_return
+#
+
+def direct_plugin_handle_http_request(request, virtual_config):
+#
+	"""
+Called for requests with the path prefix "/tasks/".
+
+:param request: Originating request instance
+:param virtual_config: Virtual path configuration
+
+:return: (object) Request object if valid
+:since:  v0.1.00
+	"""
+
+	tid = request.get_dsd("tid")
+	var_return = (None if (tid == None) else direct_hooks.call("dNG.pas.tasks.call", client = request.get_client_host(), tid = tid))
+
+	if (var_return == None):
+	#
+		direct_log_line.warning("pas.tasks refused TID '{0}'".format(tid))
+
+		var_return = direct_predefined_http_request()
+		var_return.set_module("output")
+		var_return.set_service("http")
+		var_return.set_action("error")
+		var_return.set_dsd("code", "400")
+	#
+
+	return var_return
+#
+
+def plugin_deregistration():
+#
+	"""
+Unregister plugin hooks.
+
+:since: v0.1.00
+	"""
+
+	direct_hooks.unregister("dNG.pas.http.startup", direct_plugin_http_startup)
+	direct_hooks.unregister("dNG.pas.http.wsgi.startup", direct_plugin_http_startup)
+#
+
+def plugin_registration():
+#
+	"""
+Register plugin hooks.
+
+:since: v0.1.00
+	"""
+
+	direct_hooks.register("dNG.pas.http.startup", direct_plugin_http_startup)
+	direct_hooks.register("dNG.pas.http.wsgi.startup", direct_plugin_http_startup)
+#
+
+##j## EOF
